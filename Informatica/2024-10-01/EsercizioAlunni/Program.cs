5afea84d46be33e85f8221fe165f92a285773db5
Marco Balducci <balduccimarco2007@gmail.com>
2024-10-03 23:21:31 +0200

    Refactoring di 2024-10-01

--- Informatica/2024-10-01/EsercizioAlunni/Program.cs
+++ Informatica/2024-10-01/EsercizioAlunni/Program.cs
@@ -3,6 +3,7 @@
  * Esercizio ripasso Lettura da File e class
 */
 
+using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
@@ -18,56 +19,54 @@ namespace EsercizioAlunni
 
     class Istituto
     {
+        //Dictionary per memorizzare le classi
         private Dictionary<string, Classe> _classi = new Dictionary<string, Classe>();
 
-        //metodi statici per ottenere le classi istanziate in precedenza
+        //Metodi per accedere al Dictionary
+        public bool EsisteClasse(string nomeClasse) => _classi.ContainsKey(nomeClasse.ToUpper());
 
-        public bool AlreadyInstantiated(string nomeClasse) => _classi.ContainsKey(nomeClasse.ToUpper());
+        public Classe GetClasse(string nomeClasse) => _classi[nomeClasse.ToUpper()];
 
-        public Classe GetInstance(string nomeClasse) => _classi[nomeClasse.ToUpper()];
+        public List<Classe> GetClassiI() => _classi.Values.ToList();
 
-        public List<Classe> GetClassiIstanziate() => _classi.Values.ToList();
-
-        public void Add(Classe classe) => _classi.Add(classe.NomeClasse, classe);
+        public void AddClasse(Classe classe) => _classi.Add(classe.NomeClasse, classe);
     }
 
     class Classe
     {
-        public int Anno { get; private set; }
-        public char Sezione { get; private set; }
-        public string NomeClasse { get { return $"{Anno}{Sezione}"; } }
-        public Indirizzo Indirizzo { get; private set; }
+        //variabili private
+        private int _anno;
+        private char _sezione;
         private List<Alunno> _iscritti;
 
+        //Proprietà
+        public int Anno { get { return _anno; } private set { 
+                if (value < 1 || value > 5)
+                    throw new ArgumentException("Anno non valido"); 
 
+                _anno = value;
+            } 
+        }
+        public char Sezione { get { return _sezione; } private set {
+                value = $"{value}".ToUpper()[0];
+
+                if (value < 65 || value > 90)
+                    throw new ArgumentException("Sezione non valida");
+
+                _sezione = value;
+            } 
+        }
+        public string NomeClasse { get { return $"{_anno}{_sezione}".ToUpper(); } }
+        public Indirizzo Indirizzo { get; private set; }
 
         //costruttore
-
-        public Classe(byte anno, char sezione, Indirizzo indirizzo, Istituto istitutoDiAppartenenza)
+        public Classe(byte anno, char sezione, Indirizzo indirizzo)
         {
-            //controllo errori parametri
-            if (anno < 1 || anno > 5) throw new Exception("Invalid Anno");
-            if (sezione < 65 || sezione > 90) //controllo se sezione è valida e maiuscola
-            {
-                if (sezione < 97 || sezione > 122) //controllo se sezione è valida e minuscola
-                    throw new Exception("Invalid char for sezione");
-                else
-                    sezione = (char)(sezione - 32); //sezione è resa maiuscola se minuscola
-            }
-
-            //assegnazioni valori
             Anno = anno;
             Sezione = sezione;
             Indirizzo = indirizzo;
             _iscritti = new List<Alunno>();
-
-            if (!istitutoDiAppartenenza.AlreadyInstantiated(NomeClasse))
-                istitutoDiAppartenenza.Add(this);
-            else 
-                throw new Exception("Classe already instantiated");
         }
-
-
 
         //metodi pubblici
 
@@ -79,28 +78,6 @@ namespace EsercizioAlunni
         public List<Alunno> GetAlunni() => _iscritti.GetRange(0, _iscritti.Count);
 
         public override string ToString() => $"{NomeClasse} - {Indirizzo}";
-
-
-
-        //metodi statici
-
-        public static Indirizzo ParseIndirizzo(string value)
-        {
-            switch (value.ToLower())
-            {
-                case "informatica":
-                    return Indirizzo.Informatica;
-
-                case "automazione":
-                    return Indirizzo.Automazione;
-
-                case "biotecnologie":
-                    return Indirizzo.Biotecnologie;
-
-                default:
-                    throw new Exception("Invalid Indirizzo");
-            }
-        }
 
     }
 
@@ -135,9 +112,27 @@ namespace EsercizioAlunni
     }
     internal class Program
     {
+        static Indirizzo ParseIndirizzo(string value)
+        {
+            switch (value.ToLower())
+            {
+                case "informatica":
+                    return Indirizzo.Informatica;
+
+                case "automazione":
+                    return Indirizzo.Automazione;
+
+                case "biotecnologie":
+                    return Indirizzo.Biotecnologie;
+
+                default:
+                    throw new ArgumentException("Indirizzo non valido");
+            }
+        }
+
         static void Main()
         {
-            Istituto Pascal = new Istituto();
+            Istituto istituto = new Istituto();
 
             //Istanzio StreamReader per leggere da file
             using (StreamReader sr = new StreamReader(@"..\..\..\elenco-alunni-classi.txt"))
@@ -153,21 +148,33 @@ namespace EsercizioAlunni
                         if (lineaLetta.Length != 6) throw new Exception();
 
                         //ottengo istanza della classe (quella già istanziata se esistente oppure una nuova)
-                        Classe classe = Pascal.AlreadyInstantiated($"{lineaLetta[4]}") ? Pascal.GetInstance($"{lineaLetta[4]}") : new Classe(byte.Parse($"{lineaLetta[4][0]}"), lineaLetta[4][1], Classe.ParseIndirizzo(lineaLetta[5]), Pascal);
+                        Classe classe;
+                        if (istituto.EsisteClasse(lineaLetta[4]))
+                        {
+                            classe = istituto.GetClasse(lineaLetta[4]);
+                        }
+                        else
+                        {
+                            //se non esiste la classe la creo e la aggiungo all'istituto
+                            classe = new Classe(byte.Parse($"{lineaLetta[4][0]}"), lineaLetta[4][1], ParseIndirizzo(lineaLetta[5]));
+                            istituto.AddClasse(classe);
+                        }
+
+                        //aggiungo alunno alla classe
                         classe.AddAlunno(new Alunno(lineaLetta[0], lineaLetta[1], lineaLetta[2][0], DateOnly.Parse(lineaLetta[3])));
                     }
                     catch(Exception e) //Aggiungo Alunno alla classe, se viene tirato un errore il catch termina la lettura
                     {
                         //Messaggio di Errore e stop lettura
                         Console.WriteLine(e);
-                        Console.WriteLine($"Error reading at line {linea}");
+                        Console.WriteLine($"Errore nella linea numero {linea}, stop lettura");
                         sr.Close();
                     }
                 }
             }
 
             //Stampa a video dei dati letti
-            foreach (Classe classe in Pascal.GetClassiIstanziate())
+            foreach (Classe classe in istituto.GetClassiI())
             {
                 Console.WriteLine($"\nClasse {classe}\n");
                 foreach (Alunno alunno in classe.GetAlunni())
