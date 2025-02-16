b640eb146ba738d4fda69e898c890ad111ef7428
Marco Balducci <balduccimarco2007@gmail.com>
2025-02-16 20:49:04 +0100

    added comments to website and completed ponteLevatoio

--- TecnProg/2025-01-27/Ponte Levatoio/Ponte Levatoio/Program.cs
+++ TecnProg/2025-01-27/Ponte Levatoio/Ponte Levatoio/Program.cs
@@ -14,12 +14,14 @@ namespace Ponte_Levatoio
         const int MAX_AUTO_SUL_PONTE = 4;
         const int CONSOLE_LENGTH = 115;
         const int CONSOLE_HEIGHT = 29;
+        const int MAX_WAITING_AUTO = 10;
 
         static SemaphoreSlim _sem = new SemaphoreSlim(MAX_AUTO_SUL_PONTE);
         static List<Thread> Waiting_Threads = new List<Thread>();
         static Thread[] Active_Threads = new Thread[MAX_AUTO_SUL_PONTE];
         static int[] positions = new int[MAX_AUTO_SUL_PONTE];
         static int auto_counter = 0;
+        static int waiting_auto_counter = 0;
 
         static Object _lock = new Object();
         static Random rnd = new Random();
@@ -64,6 +66,23 @@ namespace Ponte_Levatoio
             }
         }
 
+        static void StampaComandi()
+        {
+            lock (_lock)
+            {
+                SetCursorPosition(90, 1);
+                Write("COMMANDS");
+                SetCursorPosition(90, 2);
+                Write("<A> Add car");
+                SetCursorPosition(90, 3);
+                Write("<O> Open bridge");
+                SetCursorPosition(90, 4);
+                Write("<C> Close bridge");
+                SetCursorPosition(90, 5);
+                Write("<Q> Quit");
+            }
+        }
+
         static void ApriPonte()
         {
             ponteChiuso = false;
@@ -95,6 +114,8 @@ namespace Ponte_Levatoio
 
         static void ChiudiPonte()
         {
+            if (ponteChiuso) return;
+
             ponteChiuso = true;
 
             lock (_lock)
@@ -116,6 +137,9 @@ namespace Ponte_Levatoio
 
         static void AggiungiAuto()
         {
+            if(waiting_auto_counter >= MAX_WAITING_AUTO) return;
+
+            waiting_auto_counter += 1;
             Thread new_thread = new Thread(Auto);
             new_thread.Name = $"Auto {auto_counter++}";
             Waiting_Threads.Add(new_thread);
@@ -137,7 +161,7 @@ namespace Ponte_Levatoio
             }
         }
 
-        static bool checkAutoSulPonte()
+        static bool CheckAutoSulPonte()
         {
             bool auto_sul_ponte = false;
             for(int i = 0; i < Active_Threads.Length; i++)
@@ -145,10 +169,24 @@ namespace Ponte_Levatoio
                 if (Active_Threads[i] is null || Active_Threads[i].ThreadState == ThreadState.Stopped) continue;
 
                 int current_Thread_Name_Length = Active_Threads[i].Name.Length;
-                if(positions[i] + current_Thread_Name_Length > 40 || positions[i] + current_Thread_Name_Length < 64) auto_sul_ponte = true;
+                if(positions[i] + current_Thread_Name_Length > 40 && positions[i] + current_Thread_Name_Length < 80) auto_sul_ponte = true;
             }
 
             return auto_sul_ponte;
+        }
+
+        static void Quit()
+        {
+            for (int i = 0; i < Waiting_Threads.Count; i++)
+            {
+                Waiting_Threads[i].Abort();
+            }
+
+            for (int i = 0; i < Active_Threads.Length; i++)
+            {
+                if (Active_Threads[i] is null || Active_Threads[i].ThreadState == ThreadState.Stopped) continue;
+                Active_Threads[i].Abort();
+            }
         }
 
         static void Auto()
@@ -158,6 +196,7 @@ namespace Ponte_Levatoio
 
             Waiting_Threads.RemoveAt(Waiting_Threads.IndexOf(Thread.CurrentThread));
             UpdateThreadList();
+            waiting_auto_counter -= 1;
 
             int x = 0;
             int y = 13;
@@ -198,6 +237,7 @@ namespace Ponte_Levatoio
             CursorVisible = false;
 
             StampaMappa();
+            StampaComandi();
             ApriPonte();
 
             while (true)
@@ -217,8 +257,12 @@ namespace Ponte_Levatoio
                             break;
 
                         case 'O':
-                            if (!checkAutoSulPonte()) ApriPonte();
+                            if (!CheckAutoSulPonte()) ApriPonte();
                             break;
+
+                        case 'Q':
+                            Quit();
+                            return;
 
                         default:
                             break;
