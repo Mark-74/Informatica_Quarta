419b70aa5a3b6d345885fa2a720444a780f271c2
Mark-74 <balduccimarco2007@gmail.com>
2025-03-05 12:56:53 +0100

    added simple react project

---
+++ Teoria/react/my-bookstore/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import Button from './Button';
+import Navbar from './navbar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <header className="App-header">
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Teoria/react/my-bookstore/src/Button.js
@@ -0,0 +1,13 @@
+const Button = () =>{
+    onclick = () => {
+        console.log('click')
+    }
+
+    return (
+        <button>
+            Bottone!
+        </button>
+    )
+}
+
+export default Button
