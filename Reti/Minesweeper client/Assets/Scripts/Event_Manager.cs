4934b0b4e111ef515c3994becb7e22a408bb252b
Mark-74 <balduccimarco2007@gmail.com>
2025-03-23 21:49:26 +0100

    added coloring to minesweeper

--- Reti/Minesweeper client/Assets/Scripts/Event_Manager.cs
+++ Reti/Minesweeper client/Assets/Scripts/Event_Manager.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
 using static UnityEditor.PlayerSettings;
+using UnityEngine.UI;
 
 public class Event_Manager : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class Event_Manager : MonoBehaviour
         }
     }
 
+    public static Color[] BombsNumberToColors = { Color.gray, Color.green, Color.yellow, Color.red, Color.magenta, Color.cyan, Color.blue, Color.black }; //TODO: add another color for 8 bombs and change current colors
+
     public static void OnClicked(Position p)
     {
         Debug.Log($"Event Manager called with parameters {p.X},{p.Y}");
@@ -26,21 +29,21 @@ public class Event_Manager : MonoBehaviour
     public static void OnServerResponse(Position move, Socket_Manager.ServerGameData data)
     {
         Debug.Log(data);
-        if(data.HasWon)
-        {
-            Debug.Log("You won!");
-            return;
-        }
         if (data.HasLost)
         {
             Debug.Log("You lost!");
-            GameObject.Find($"Square {move.X}, {move.Y}").gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            GameObject.Find($"Square {move.X}, {move.Y}").gameObject.GetComponent<SpriteRenderer>().color = Color.black;
             return;
         }
         foreach (Socket_Manager.ServerPosition pos in data.OpenedCells)
         {
             Debug.Log($"Square {pos.X}, {pos.Y}");
-            GameObject.Find($"Square {pos.X}, {pos.Y}").gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            GameObject.Find($"Square {pos.X}, {pos.Y}").gameObject.GetComponent<SpriteRenderer>().color = BombsNumberToColors[pos.Adjacent];
+        }
+        if (data.HasWon)
+        {
+            Debug.Log("You won!");
+            return;
         }
     }
 
